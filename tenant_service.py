bcaf9c44dac37776b66b75b2e483d2512a3f9525
Francisco de la Vega <fdelavega@ficodes.com>
2019-05-07 19:32:38 +0200

    Add basic plugin structure

---
+++ tenant_service.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2019 Future Internet Consulting and Development Solutions S.L.
+
+# This file is part of BAE Tenant Service plugin.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from wstore.asset_manager.resource_plugins.plugin import Plugin
+
+from settings import TENANT_MANAGER, ACCESS_ROLE, UNITS
+
+
+class TenantService(Plugin):
+
+    def __init__(self, plugin_model):
+        super(TenantService, self).__init__(plugin_model)
+        self._units = UNITS
+
+    def on_post_product_spec_validation(self, provider, asset):
+        pass
+
+    def on_post_product_offering_validation(self, asset, product_offering):
+        # Validate that the pay-per-use model (if any) is supported by the backend
+        if 'productOfferingPrice' in product_offering:
+            has_usage = False
+            supported_units = [unit['name'].lower() for unit in self._units]
+
+            for price_model in product_offering['productOfferingPrice']:
+                if price_model['priceType'] == 'usage':
+                    has_usage = True
+
+                    if price_model['unitOfMeasure'].lower() not in supported_units:
+                        raise PluginError('Unsupported accounting unit ' +
+                                          price_model['unit'] + '. Supported units are: ' + ','.join(supported_units))
+
+    def on_product_acquisition(self, asset, contract, order):
+        pass
+
+    def on_product_suspension(self, asset, contract, order):
+        pass
+
+    def get_usage_specs(self):
+        return self._units
+
+    def get_pending_accounting(self, asset, contract, order):
+        accounting = []
+        last_usage = None
+        # Read pricing model to know the query to make
+        if 'pay_per_use' in contract.pricing_model:
+            unit = contract.pricing_model['pay_per_use'][0]['unit']
+
+            # Read the date of the last SDR
+            if contract.last_usage is not None:
+                start_at = unicode(contract.last_usage.isoformat()).replace(' ', 'T') + 'Z'
+            else:
+                # The maximum time between refreshes is 30 days, so in the worst case
+                # consumption started 30 days ago
+                start_at = unicode((datetime.utcnow() - timedelta(days=31)).isoformat()).replace(' ', 'T') + 'Z'
+
+            # Retrieve pending usage
+            last_usage = datetime.utcnow()
+            end_at = unicode(last_usage.isoformat()).replace(' ', 'T') + 'Z'
+
+            # Check the accumulated usage for all the resources of the dataset
+            # Accounting is always done by Umbrella no mather who validates permissions
+            
+
+        return accounting, last_usage
